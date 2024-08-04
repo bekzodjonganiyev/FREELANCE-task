6a9782165c258937dbf0d6967b32153f2786f0e4
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2024-08-04 15:27:28 +0500

    defaults changed

---
+++ src/utils/mock-data.ts
@@ -0,0 +1,52 @@
+export const catalogData = [
+    {
+        id: 1,
+        name: "Xiomi",
+        slug: "xiomi"
+    },
+    {
+        id: 2, 
+        name: "Avatr",
+        slug: "avatr"
+    },
+    {
+        id: 3, 
+        name: "Lixiang",
+        slug: "lixiang"
+    },
+    {
+        id: 4, 
+        name: "Zeekr",
+        slug: "zeekr"
+    },
+    {
+        id: 5, 
+        name: "Polistone",
+        slug: "polistone"
+    },
+    {
+        id: 6, 
+        name: "Voyah",
+        slug: "voyah"
+    },
+    {
+        id: 7, 
+        name: "Hiphi",
+        slug: "hiphi"
+    },
+    {
+        id: 8, 
+        name: "Aito",
+        slug: "aito"
+    },
+    {
+        id: 9, 
+        name: "Lotus",
+        slug: "lotus"
+    },
+    {
+        id: 10, 
+        name: "Yangwang",
+        slug: "yangwang"
+    },
+]
